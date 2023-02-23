f32509b61b88e4123712554317894271910a25c4
Qadar <gq2853248@gmail.com>
2023-02-24 00:57:29 +0500

    testcase1

--- testcase/demotc1.py
+++ testcase/demotc1.py
@@ -1,2 +1,16 @@
 import selenium
-from
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class demotc1():
+    def demo_testcase1():
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver.get("https://www.google.com")
+        print(driver.title)
+        driver.close()
+        var=democt1()
+        var.demo_testcase1()
